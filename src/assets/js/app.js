18027f468b7c76f84a6c2afd049d95c0afcabdac
Cesar Danilo <billypalacios616@gmail.com>
2023-03-06 22:38:06 -0300

    PrimeiroCommit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -6,6 +6,8 @@ const pTimer = document.querySelector('.timer');
 let segundos = 0;
 let timers;
 
+//Primeiro vamos crirar a função que vai retornar nosso cronometro formatado
+//Nele vamos passar um parametro que vai receber os segundos
 function cronometro(segundos) {
 	const temp = new Date(segundos * 1000); // dividindo por 1000 para transformar em milesimos
 	return temp.toLocaleTimeString('pt-br', { // toLocaleTimeString vai formatar a nosso cronometro como horio
@@ -15,6 +17,7 @@ function cronometro(segundos) {
 }
 
 btnIniciar.addEventListener('click', function (event){
+	clearInterval(timers); // passando esse comando, se clicarmos mais vezes em iniciar não irar acelerar o tempo do cronometro, ele vai apenas trazar e continuar normalmente
 	timers = setInterval(function(){
 		segundos++;
 		pTimer.innerHTML = cronometro(segundos);
@@ -23,4 +26,9 @@ btnIniciar.addEventListener('click', function (event){
 
 btnPausar.addEventListener('click', function (event){
 	clearInterval(timers); // clearInterval vai parar qualquer função de repetição que receba o setInterval()
+});
+
+btnResetar.addEventListener('click', function(event){
+	segundos = 0;
+	pTimer.innerHTML = cronometro(segundos);
 });
